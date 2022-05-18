2e0cdfe37b68370db5e68cdbf51f527bd2a75d6c
赖守美 <441974767@qq.com>
2022-05-18 17:49:08 +0800

    feat: 封装操作按钮组件

--- src/components/ComProtable/MenuProTable/menuButton.tsx
+++ src/components/ComProtable/MenuProTable/menuButton.tsx
@@ -1,22 +1,45 @@
 import React from 'react'
 import { Button } from 'antd'
-import { FileAddOutlined } from '@ant-design/icons'
+import { PlusOutlined, EditOutlined, DeleteOutlined } from '@ant-design/icons'
 
 import styles from './index.less'
 
-const MenuButton: React.FC<any> = ({ children, onClick }) => {
+const style = { marginLeft: '3px' }
+// 新增
+const MenuAddButton: React.FC<any> = ({ children, onClick }) => {
   return (
-    <>
-      <Button
-        type="primary"
-        className="add-left-btn"
-        onClick={onClick}
-      >
-        <FileAddOutlined />
-        <span className={`${styles['yue-pro-table-btn']}`}>{children}</span>
-      </Button>
-    </>
+    <Button type="primary" onClick={onClick}>
+      <PlusOutlined />
+      <span className={`${styles['yue-pro-table-btn']}`}>{children ? children : '新增'}</span>
+    </Button>
+  )
+}
+// 批量删除
+const MenuMultiDelButton: React.FC<any> = ({ children, onClick }) => {
+  return (
+    <Button type="primary" onClick={onClick}>
+      <DeleteOutlined />
+      <span className={`${styles['yue-pro-table-btn']}`}>{children ? children : '批量删除'}</span>
+    </Button>
+  )
+}
+// 修改
+const MenuEditButton: React.FC<any> = ({ children, onClick }) => {
+  return (
+    <a onClick={onClick}>
+      <EditOutlined />
+      <span style={style}>{children ? children : '修改'}</span>
+    </a>
+  )
+}
+// 删除
+const MenuDelteButton: React.FC<any> = ({ children, onClick }) => {
+  return (
+    <a onClick={onClick}>
+      <DeleteOutlined />
+      <span style={style}>{children ? children : '删除'}</span>
+    </a>
   )
 }
 
-export default MenuButton
+export { MenuAddButton, MenuMultiDelButton, MenuEditButton, MenuDelteButton }
