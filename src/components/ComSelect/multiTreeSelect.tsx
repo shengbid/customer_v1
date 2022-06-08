55ae09f030f194286d7f604a414fa44481c62a0f
赖守美 <441974767@qq.com>
2022-06-08 18:03:13 +0800

    feat: 树形多选增加父子选中关联功能

--- src/components/ComSelect/multiTreeSelect.tsx
+++ src/components/ComSelect/multiTreeSelect.tsx
@@ -3,9 +3,10 @@ import { TreeSelect } from 'antd'
 import { getMenuTreeData, getDeptTreeList } from '@/services'
 import { handleTreeData } from '@/utils/base'
 import { useIntl } from 'umi'
+import { find, uniq } from 'lodash'
 export interface treeSelectProps {
   placeholder?: string
-  value?: string
+  value?: any
   treeCheckStrictly?: boolean
   type?: string // 接口类型 默认菜单
   onChange?: (value: string) => void
@@ -20,18 +21,20 @@ export interface treeProps {
   key: number | string
   children: treeItemProps[]
 }
+// 多选下拉组件
 const MultiTreeDataSelect: React.FC<treeSelectProps> = (props) => {
   const [treeData, setTreeData] = useState<treeProps[]>([])
   const intl = useIntl()
   const {
     value,
-    onChange,
+    onChange = () => {},
     type = '0',
     treeCheckStrictly = true,
     placeholder = `${intl.formatMessage({
       id: 'pages.form.select',
     })}`,
   } = props
+  const [values, setValues] = useState<string[]>(value || [])
 
   const getList = async () => {
     let data: any = {}
@@ -48,6 +51,65 @@ const MultiTreeDataSelect: React.FC<treeSelectProps> = (props) => {
     }
     setTreeData(handleTreeData(data.data, 'id', 'label'))
   }
+  // 点击/取消 节点有子节点,将子节点也选中/取消选中,
+  const handleTree = (data: any, selectType: number) => {
+    let newValue: any
+    const ids: any[] = []
+    const render = (arr: any) => {
+      arr.forEach((item: any) => {
+        if (item.children) {
+          render(item.children)
+        }
+        ids.push(item.value)
+      })
+    }
+    render(data)
+    if (selectType === 1) {
+      newValue = uniq(values?.concat(ids)) // 去重,连接
+    } else {
+      newValue = values?.filter((item) => !ids.includes(item))
+    }
+    // console.log(33, data, newValue)
+    setValues(newValue)
+    onChange(newValue)
+  }
+  const changeTreeNode = (option: any, label: any, extra: any) => {
+    let ids: any[] = []
+    let currentItem: any = {}
+    let selectType = 1 // 1选中 2取消选中
+    if (option.length > extra.preValue.length) {
+      // 选中
+      currentItem = option.pop()
+    } else {
+      // 取消选中
+      selectType = 2
+      ids = option.map((item: any) => item.value)
+      currentItem = find(extra.preValue, (item: any) => !ids.includes(item.value))
+    }
+
+    let selectTree: any[] = []
+    const render = (data: any) => {
+      data.forEach((item: any) => {
+        if (item.value === currentItem.value) {
+          if (item.children) {
+            selectTree = item.children
+          }
+        } else if (item.children) {
+          render(item.children)
+        }
+      })
+    }
+    render(treeData)
+    handleTree([...selectTree, currentItem], selectType)
+    // setValues(option.map((item: any) => item.value))
+    // onChange(option)
+  }
+
+  useEffect(() => {
+    if (value) {
+      setValues(value)
+    }
+  }, [value])
 
   useEffect(() => {
     getList()
@@ -57,7 +119,7 @@ const MultiTreeDataSelect: React.FC<treeSelectProps> = (props) => {
     <TreeSelect
       showSearch
       style={{ width: '100%' }}
-      value={value}
+      value={values}
       dropdownStyle={{ maxHeight: 400, overflow: 'auto' }}
       placeholder={placeholder}
       allowClear
@@ -66,7 +128,7 @@ const MultiTreeDataSelect: React.FC<treeSelectProps> = (props) => {
       treeCheckStrictly={treeCheckStrictly}
       // treeDefaultExpandAll
       maxTagCount={7}
-      onChange={onChange}
+      onChange={changeTreeNode}
       treeData={treeData}
     />
   )
