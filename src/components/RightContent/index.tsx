4e09713270b897e720d5baf475a61965deb0ebc0
赖守美 <441974767@qq.com>
2022-06-27 10:38:41 +0800

    fix: 修复登录异常个人信息显示问题

--- src/components/RightContent/index.tsx
+++ src/components/RightContent/index.tsx
@@ -1,19 +1,20 @@
 import { Space } from 'antd'
 import React from 'react'
-import { useModel, SelectLang } from 'umi'
+import { /*useModel,*/ SelectLang } from 'umi'
 import Avatar from './AvatarDropdown'
 import styles from './index.less'
+import defaultSettings from '../../../config/defaultSettings'
 
 export type SiderTheme = 'light' | 'dark'
 
 const GlobalHeaderRight: React.FC = () => {
-  const { initialState } = useModel('@@initialState')
+  // const { initialState } = useModel('@@initialState')
 
-  if (!initialState || !initialState.settings) {
-    return null
-  }
+  // if (!initialState || !initialState.settings) {
+  //   return null
+  // }
 
-  const { navTheme, layout } = initialState.settings
+  const { navTheme, layout } = defaultSettings
   let className = styles.right
 
   if ((navTheme === 'dark' && layout === 'top') || layout === 'mix') {
